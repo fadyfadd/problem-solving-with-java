93b5994256358e4cf313fc9f5d9787239c0191e0
fadyfadd <fady_faddoul@hotmail.com>
2025-09-24 13:51:30 +0300

    printFibonacci correction

--- FibonacciPrinter.java
+++ FibonacciPrinter.java
@@ -1,19 +1,23 @@
 public class FibonacciPrinter {
 
     public static void printFibonacci(int nbr) {
+        if (nbr <= 0) {
+            System.out.println("No Fibonacci numbers to display.");
+            return;
+        }
+
         int a1 = 0;
         int a2 = 1;
-        int sum = a1 + a2;
 
         for (int i = 0; i < nbr; i++) {
-            System.out.println(sum);
+            System.out.println(a1);
+            int sum = a1 + a2;
             a1 = a2;
             a2 = sum;
-            sum = a1 + a2;
         }
     }
 
     public static void main(String[] args) {
-        printFibonacci(10); 
+        printFibonacci(10);
     }
 }
